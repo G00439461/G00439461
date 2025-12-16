c9c21f07a2b9221562a5cd30685a9c2cfb21f229
G00439461 <G00439461@atu.ie>
2025-12-16 14:32:08 +0000

    Add recipe details navigation from search results

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
       import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'recipe/:id',
+  loadComponent: () =>
+    import('./recipe-details/recipe-details.page').then((m) => m.RecipeDetailsPage),
+  },
+  {
     path: 'settings',
     loadComponent: () =>
       import('./settings/settings.page').then((m) => m.SettingsPage),
